9f5250f8a835cfe70ff5046a1a2bbf0afb1509a0
Denis Gorodnichev <denis.gorodnichev@gmail.com>
2016-11-26 09:04:37 +0300

    wm-tester: support gtk 3.22 api

--- src/wm-tester/main.c
+++ src/wm-tester/main.c
@@ -118,13 +118,28 @@ evil_timeout (gpointer data)
       int t;
       GtkWidget *parent;
 
+#if GTK_CHECK_VERSION (3, 22, 0)
+      GdkWindow *window;
+      GdkMonitor *monitor;
+      GdkRectangle geometry = {0, 0, 0, 0};
+#endif
+
       w = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 
+#if GTK_CHECK_VERSION (3, 22, 0)
+      window = gtk_widget_get_window (w);
+      monitor = gdk_display_get_monitor_at_window (gtk_widget_get_display (w), window);
+      gdk_monitor_get_geometry (monitor, &geometry);
+      gtk_window_move (GTK_WINDOW (w),
+                       g_random_int_range (geometry.x, geometry.x + geometry.width),
+                       g_random_int_range (geometry.y, geometry.y + geometry.height));
+#else
       gtk_window_move (GTK_WINDOW (w),
                        g_random_int_range (0,
                                            gdk_screen_width ()),
                        g_random_int_range (0,
                                            gdk_screen_height ()));
+#endif
 
       parent = NULL;
 
